28e63d45d957915a46e1735999750cf6f2fffbcd
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-09-09 16:54:22 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,11 +2,11 @@
 # out the current state.def display_board (board)
 
 def display_board(board)
-  puts "#{board[0]}|#{board[1]}|#{board[2]}"
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
-  puts "#{board[4]}|#{board[5]} |   "
+  puts "#{board[4]} | #{board[5]} | #{board[6]}"
   puts "-----------"
-  puts "   |   |   "
+  puts "#{board[7]} | #{board[8]} | #{board[9]}"
 end
 
 board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
